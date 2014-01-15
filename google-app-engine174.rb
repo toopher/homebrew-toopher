ecb3b89b89e55e52e997bc53e0d358de2e26055a
Thomas Darr <me@trdarr.com>
2014-01-15 11:40:12 -0600

    google-app-engine 1.7.4: use Dropbox link
    GAE 1.7.4 is deprecated and no longer hosted by Google, so until we come up
    with a better hosting solution, it lives in @trdarr's Dropbox.

--- google-app-engine174.rb
+++ google-app-engine174.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class GoogleAppEngine < Formula
   homepage 'https://developers.google.com/appengine/'
-  url 'http://googleappengine.googlecode.com/files/google_appengine_1.7.4.zip'
+  url 'https://www.dropbox.com/s/1lgegp625r4kqx3/google_appengine_1.7.4.zip?dl=1'
   sha1 '05eaddb534e3cb1d002cdaa529ff75ef36a30026'
 
   def install
