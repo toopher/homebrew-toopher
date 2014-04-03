9dcccd43658650ef7cd774e743c4aa32521ab961
Thomas Darr <me@trdarr.com>
2014-04-03 14:59:41 -0500

    PivotalBooster: an OS X client for Pivotal Tracker

---
+++ pivotalbooster.rb
@@ -0,0 +1,7 @@
+class PivotalBooster < Cask
+  url 'http://pivotalbooster.com/downloads/releases/1.1.4.beta/PivotalBooster_1.1.4.beta.dmg'
+  homepage 'http://pivotalbooster.com/'
+  version '1.1.4(beta)'
+  sha1 '443dece165ce9de2483ded10d418372f41917528'
+  link 'PivotalBooster.app'
+end
